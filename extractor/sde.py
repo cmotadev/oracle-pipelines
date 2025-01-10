05351287cf4b1066aff995871a191414b9694f00
cmotadev <cmota.dev@gmail.com>
2025-01-10 19:31:01 -0300

    Método para verificar a versão do SDE do banco de dados

--- extractor/sde.py
+++ extractor/sde.py
@@ -9,3 +9,21 @@ class FeatureClassExtractor:
         self._engine = engine
         self._schema = schema
         self._schema = table
+
+    @property
+    def database_name(self):
+        return self._engine.dialect.name
+
+    def sde_database_version(self):
+        with self._engine.connect() as conn:
+            _table = 'sde.sde_version'
+
+            if conn.dialect.name == 'oracle':
+                _table = 'sde.version'        
+
+            major, minor, bugfix = conn.execute(
+                sa.text(f"SELECT major, minor, bugfix FROM {_table}"),
+                
+            ).fetchone()
+
+        return major, minor, bugfix
